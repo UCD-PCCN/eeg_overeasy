ba2a462abad3f3d358bdcd50f78d833e58c0c213
pwitkow <pwitkowski@ucdavis.edu>
2018-06-08 10:03:17 -0700

    removed functional redundancies in boot class and; default channels to all eeg nodes; plot method updated to plot boot_SE; eegpwr in module form and example script added

---
+++ .ipynb_checkpoints/untitled-checkpoint.py
@@ -0,0 +1,12 @@
+#imports 
+import mne
+import numpy as np
+import pandas as pd
+import os
+import glob
+from matplotlib import pyplot as plt
+import sys
+import warnings
+
+import seaborn as sns
+from itertools import product as prod
